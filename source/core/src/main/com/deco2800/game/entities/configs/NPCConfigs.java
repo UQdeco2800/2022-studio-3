d0fc49bd175890e3110328ad507abcce1fbd85c1
Harikesha Suresh <harikeshasuresh@gmail.com>
2022-08-23 20:47:56 +1000

    Merged commit from #34 into my branch and resolved some merge conflicts.

--- source/core/src/main/com/deco2800/game/entities/configs/NPCConfigs.java
+++ source/core/src/main/com/deco2800/game/entities/configs/NPCConfigs.java
@@ -4,6 +4,6 @@ package com.deco2800.game.entities.configs;
  * Defines all NPC configs to be loaded by the NPC Factory.
  */
 public class NPCConfigs {
-  public BaseCombatEntityConfig ghoul = new BaseCombatEntityConfig();
-  public BaseCombatEntityConfig demon = new BaseCombatEntityConfig();
+  public BaseEntityConfig ghoul = new BaseEntityConfig();
+  public BaseEntityConfig demon = new BaseEntityConfig();
 }
--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -10,9 +10,7 @@ import com.deco2800.game.components.TouchAttackComponent;
 //import com.deco2800.game.components.tasks.ChaseTask;
 import com.deco2800.game.components.tasks.WanderTask;
 import com.deco2800.game.entities.Entity;
-import com.deco2800.game.entities.configs.BaseCombatEntityConfig;
-//import com.deco2800.game.entities.configs.BaseEntityConfig;
-//import com.deco2800.game.entities.configs.GhostKingConfig;
+import com.deco2800.game.entities.configs.BaseEntityConfig;
 import com.deco2800.game.entities.configs.NPCConfigs;
 import com.deco2800.game.files.FileLoader;
 import com.deco2800.game.physics.PhysicsLayer;
@@ -45,7 +43,7 @@ public class NPCFactory {
    */
   public static Entity createGhoul() {
     Entity ghoul = createBaseNPC();
-    BaseCombatEntityConfig config = configs.ghoul;
+    BaseEntityConfig config = configs.ghoul;
 
     AnimationRenderComponent animator =
         new AnimationRenderComponent(
@@ -54,8 +52,8 @@ public class NPCFactory {
     animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
 
     ghoul
-        .addComponent(new CombatStatsComponent(config.baseHealth, config.baseAttack,
-            config.baseDefence, config.baseSpeed)) // casting to int temporary!
+        .addComponent(new CombatStatsComponent(config.health, config.baseAttack,
+            config.baseDefence)) // casting to int temporary!
         .addComponent(animator)
         .addComponent(new GhostAnimationController());
 
@@ -71,7 +69,7 @@ public class NPCFactory {
    */
   public static Entity createDemon() {
     Entity demon = createBaseNPC();
-    BaseCombatEntityConfig config = configs.demon;
+    BaseEntityConfig config = configs.demon;
 
     AnimationRenderComponent animator =
         new AnimationRenderComponent(
@@ -80,8 +78,8 @@ public class NPCFactory {
     animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
 
     demon
-        .addComponent(new CombatStatsComponent(config.baseHealth, config.baseAttack,
-            config.baseDefence, config.baseSpeed)) // casting to int temporary!
+        .addComponent(new CombatStatsComponent(config.health, config.baseAttack,
+            config.baseDefence)) // casting to int temporary!
         .addComponent(animator)
         .addComponent(new GhostAnimationController());
 
