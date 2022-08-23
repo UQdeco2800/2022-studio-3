55912a97ed03f30c3c5cf1500879e8a3a2127b97
Corfliss <fauzanhanandito@gmail.com>
2022-08-23 17:12:29 +1000

    Temporary file of TimeBasedRandom to edit with

---
+++ source/core/src/main/com/deco2800/game/utils/random/TimeBasedRandom.java
@@ -0,0 +1,30 @@
+package com.deco2800.game.utils.random;
+
+import com.deco2800.game.utils.random.PseudoRandom;
+import java.time.*;
+
+public class TimeBasedRandom {
+
+    // Make Java constructor (just in case)
+    public LocalTime get_time() {
+        return time;
+    }
+
+    public void set_time(LocalTime time) {
+        this.time = time;
+    }
+
+    public void random_time_inteval(intervalSeconds) {
+        // Message to Jordan:
+        // Can you make random generator for long type? LocalTime needs it.
+        // Thanks,
+        // Dito
+
+        long longRandomSeconds = seedRandomLong(0, intervalSeconds);
+        return longRandomSeconds
+    }
+
+    // For now it is what it is
+
+    // TODO: Import object from buildings and maps
+}
