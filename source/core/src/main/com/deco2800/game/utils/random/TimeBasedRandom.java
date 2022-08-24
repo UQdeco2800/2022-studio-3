41dde9f1da40a82c84cfd685d7ee4b31bedfefc3
Corfliss <fauzanhanandito@gmail.com>
2022-08-24 13:35:33 +1000

    Fix TimeBasedRandom for a while again

--- source/core/src/main/com/deco2800/game/utils/random/TimeBasedRandom.java
+++ source/core/src/main/com/deco2800/game/utils/random/TimeBasedRandom.java
@@ -6,23 +6,24 @@ import java.time.*;
 public class TimeBasedRandom {
 
     // Make Java constructor (just in case)
-    public LocalTime get_time() {
-        return time;
-    }
+//    public LocalTime get_time() {
+//        return time;
+//    }
+//
+//    public void set_time(LocalTime time) {
+//        this.time = time;
+//    }
 
-    public void set_time(LocalTime time) {
-        this.time = time;
-    }
-
-    public void random_time_inteval(intervalSeconds) {
+//    public void random_time_inteval(intervalSeconds) {
         // Message to Jordan:
         // Can you make random generator for long type? LocalTime needs it.
         // Thanks,
         // Dito
 
-        long longRandomSeconds = seedRandomLong(0, intervalSeconds);
-        return longRandomSeconds
-    }
+//        long longRandomSeconds = seedRandomLong(0, intervalSeconds);
+//        return longRandomSeconds
+
+    //}
 
     // For now it is what it is
 
