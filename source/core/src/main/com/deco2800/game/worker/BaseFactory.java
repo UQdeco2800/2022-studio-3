41028affa9dab5915ecc779b4989573442cc0a3b
Lorenzo Policar <lorenzo_policar@yahoo.com>
2022-08-25 14:12:30 +1000

    Fixed bug where collider box was not center of the base entity

--- source/core/src/main/com/deco2800/game/worker/BaseFactory.java
+++ source/core/src/main/com/deco2800/game/worker/BaseFactory.java
@@ -1,5 +1,6 @@
 package com.deco2800.game.worker;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.files.FileLoader;
@@ -7,6 +8,8 @@ import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.physics.PhysicsUtils;
 import com.deco2800.game.physics.components.ColliderComponent;
 import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.physics.components.PhysicsComponent.AlignX;
+import com.deco2800.game.physics.components.PhysicsComponent.AlignY;
 import com.deco2800.game.rendering.TextureRenderComponent;
 import com.deco2800.game.worker.components.ResourceStatsComponent;
 import com.deco2800.game.worker.resources.ResourceConfig;
@@ -19,12 +22,12 @@ public class BaseFactory {
             .addComponent(new TextureRenderComponent("images/base.png"))
             .addComponent(new PhysicsComponent())
             .addComponent(new ColliderComponent())
-            .addComponent(new HitboxComponent().setLayer(PhysicsLayer.OBSTACLE))
+            .addComponent(new HitboxComponent().setLayer(PhysicsLayer.DEFAULT))
             .addComponent(new ResourceStatsComponent(stats.wood, stats.stone, stats.iron));
         newBase.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
         newBase.getComponent(TextureRenderComponent.class).scaleEntity();
         newBase.scaleHeight(2.5f);
-        PhysicsUtils.setScaledCollider(newBase, 0.5f, 0.2f);
+        newBase.getComponent(ColliderComponent.class).setAsBoxAligned(new Vector2(), AlignX.CENTER, AlignY.CENTER);
         return newBase;
     }
 }
