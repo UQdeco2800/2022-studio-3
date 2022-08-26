604838e18dbe1caeb494dc76ef8d9f4e6ca26bc2
Corfliss <fauzanhanandito@gmail.com>
2022-08-26 17:08:06 +1000

    Implementing comments and updates for RandomTimer and RandomInterrupt

--- source/core/src/main/com/deco2800/game/utils/random/RandomInterrupt.java
+++ source/core/src/main/com/deco2800/game/utils/random/RandomInterrupt.java
@@ -6,13 +6,25 @@ import com.deco2800.game.services.GameTime;
 
 public class RandomInterrupt {
 
-    // Interrupt the game when the timer is reached
+    /**
+     * Interrupt the game if the timer is expired.
+     * @return timeScale to 0 if the time runs out.
+     */
     public void interruptGame() {
         RandomTimer rT = new RandomTimer(0,300); // Initial for example
         GameTime gT = new GameTime();
         if (rT.isRandomTimerExpired()) {
             gT.setTimeScale(0f); // No way to revert it back for now.
         }
+    }
+
+    /**
+     * Resume the game regardles if it interrupted or not
+     * @return timeScale to 1
+     */
+    public void resumeGame() {
+        GameTime gT = new GameTime();
+        gT.setTimeScale(1f);
     }
 }
 
--- source/core/src/main/com/deco2800/game/utils/random/RandomTimer.java
+++ source/core/src/main/com/deco2800/game/utils/random/RandomTimer.java
@@ -7,32 +7,50 @@ import com.deco2800.game.utils.random.PseudoRandom;
 
 public class RandomTimer {
 
-    // Initiating the start and delay variable
+    /**
+     * Initiating the start variable.
+     */
     long start;
+
+    /**
+     * Initiating the delay variable.
+     */
     long delay;
 
-    // Assigning the delay to the lower bound and upperbound.
+    /**
+     * Setting up random timer to the class variable.
+     * @return setting delay for the RandomTimer class.
+     */
     public RandomTimer(int lowerIntervalSeconds, int upperIntervalSeconds) {
         int randomIntervalInt = PseudoRandom.seedRandomInt(lowerIntervalSeconds, upperIntervalSeconds);
         long delay = Long.valueOf(randomIntervalInt);
         this.delay = delay;
     }
 
-    // Initiating start time
+    /**
+     * Setting up the start variable to the current time.
+     * @return setting start variable with the current time for the RandomTimer class.
+     */
     public void start() {
         this.start = System.currentTimeMillis();
     }
 
-    // Initiating if the countdown will expire or not
+    /**
+     * Checking whether or not if the timer is already expired or not.
+     * @return boolean of time expiration.
+     */
     public boolean isRandomTimerExpired() {
         return (System.currentTimeMillis() - this.start) > this.delay;
     }
 
-    // Initiating method to display on debugger how many times left if it is ready
+    /**
+     * Checking whether or not if the timer is already expired or not.
+     * @return Time left if it exists or time expiration info if it is not.
+     */
     public void timeLeft() {
         if (isRandomTimerExpired()) {
             System.out.println("Timer expired");
-        } else{
+        } else {
             System.out.println("Time left = " + (this.delay - (System.currentTimeMillis() - this.start)));
         }
     }
