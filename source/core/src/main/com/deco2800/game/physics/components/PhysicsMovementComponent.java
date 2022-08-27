31592130655ca25001ac333f018161a29ace48b7
DLAmeng <xzy1152241624@gmail.com>
2022-08-27 23:11:45 +1000

    remove movetask, and add a function to change entity speed

--- source/core/src/main/com/deco2800/game/physics/components/PhysicsMovementComponent.java
+++ source/core/src/main/com/deco2800/game/physics/components/PhysicsMovementComponent.java
@@ -11,11 +11,25 @@ import org.slf4j.LoggerFactory;
 /** Movement controller for a physics-based entity. */
 public class PhysicsMovementComponent extends Component implements MovementController {
   private static final Logger logger = LoggerFactory.getLogger(PhysicsMovementComponent.class);
-  private static final Vector2 maxSpeed = Vector2Utils.ONE;
-
+  private Vector2 maxSpeed = Vector2Utils.ONE;
   private PhysicsComponent physicsComponent;
   private Vector2 targetPosition;
   private boolean movementEnabled = true;
+
+
+
+  /**
+   * Initialising the PhysicsMovementComponent by default.
+   */
+  public PhysicsMovementComponent() {}
+
+  /**
+   * Creat a entity that have a base speed.
+   * @param speed the moving speed
+   */
+  public PhysicsMovementComponent(Vector2 speed) {
+    maxSpeed = speed;
+  }
 
   @Override
   public void create() {
