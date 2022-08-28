74df5b7b95ce28f30976fdc187b4f928da3518a4
hugh5 <hughdrummond11@gmail.com>
2022-08-29 02:04:08 +1000

    added building configs

---
+++ source/core/src/main/com/deco2800/game/entities/configs/BuildingConfigs.java
@@ -0,0 +1,7 @@
+package com.deco2800.game.entities.configs;
+
+public class BuildingConfig {
+    public BaseEntityConfig ghost = new BaseEntityConfig();
+    public GhostKingConfig ghostKing = new GhostKingConfig();
+
+}
---
+++ source/core/src/main/com/deco2800/game/entities/configs/TownHallConfig.java
@@ -0,0 +1,2 @@
+package com.deco2800.game.entities.configs;public class TownHallConfig {
+}
