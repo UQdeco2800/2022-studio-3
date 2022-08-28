0cbb500ae0602f4a0f84221f5f988fa5b419d00e
Corfliss <fauzanhanandito@gmail.com>
2022-08-28 21:11:56 +1000

    Removing the temporary files

--- source/core/src/main/com/deco2800/game/components/weather/WeatherIcon.java
+++ source/core/src/main/com/deco2800/game/components/weather/WeatherIcon.java
@@ -1,13 +1,15 @@
-package com.deco2800.game.components.weather;
-
-import com.badlogic.gdx.Gdx;
-
-public class WeathterIcon extends Actor {
-
-    /**
-     *
-     */
-    private final Image weatherImage;
-
-    public  WeathterIcon()
-}
+//package com.deco2800.game.components.weather;
+//
+//import com.badlogic.gdx.Gdx;
+//
+//public class WeatherIcon extends Actor {
+//
+//    /**
+//     *
+//     */
+//    private final Image weatherImage;
+//
+//    public  WeatherIcon(String title) {
+//        this.weatherImage = new Image(new Texture(Gdx.files.internal("images/")));
+//    }
+//}
