96f19653711a298f468a43156ec2d5baf06029f6
Corfliss <fauzanhanandito@gmail.com>
2022-08-28 23:23:42 +1000

    Added WeatherIcon dan WeatherIconDisplay

--- source/core/src/main/com/deco2800/game/components/weather/WeatherIcon.java
+++ source/core/src/main/com/deco2800/game/components/weather/WeatherIcon.java
@@ -1,52 +1,53 @@
-//package com.deco2800.game.components.weather;
-//
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.g2d.Batch;
-//import com.badlogic.gdx.scenes.scene2d.ui.Image;
-//import com.badlogic.gdx.scenes.scene2d.Actor;
-//import com.deco2800.game.utils.random.PseudoRandom;
-//
-//public class WeatherIcon extends Actor {
-//
-//    /**
-//     * Initiate the Image for the weather image.
-//     */
-//    private final Image weatherImage;
-//
-//    private final String[] weatherFile = {
-//            "cloud.png",
-//            "flood.png",
-//            "foggy.png",
-//            "heavyrain.png",
-//            "lighting.png",
-//            "night.png",
-//            "partlycloudy.png",
-//            "rainy.png",
-//            "sleet.png",
-//            "snowy.png",
-//            "stormy.png",
-//            "sunny.png",
-//            "thunderstorm.png"
-//    }
-//
-//    public WeatherIcon() {
-//        this.weatherImage = new Image(new Texture("images/"+getRandomWeatherFile()));
-//        this.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//        this.setPosition(0f, 0f);
-//        position();
-//    }
-//
-//    private void position() {
-//        weatherImage.setPosition(Gdx.graphics.getWidth()/2 - weatherImage.getWidth()/2, 1f);
-//    }
-//
-//    private int getRandomWeatherFile() {
-//        randomIndex = PseudoRandom.seedRandomInt(0, weatherFile.length);
-//        return weatherFile[randomIndex];
-//    }
-//
-//    @Override
-//    public void draw(Batch batch) {
-//        batch.draw(this.weatherImage);
-//    }
-//}
+package com.deco2800.game.components.weather;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.deco2800.game.utils.random.PseudoRandom;
+
+public class WeatherIcon extends Actor {
+
+    /**
+     * Initiate the Image for the weather image.
+     */
+    private final Image weatherImage;
+
+    private final String[] weatherFile = {
+            "cloud.png",
+            "flood.png",
+            "foggy.png",
+            "heavyrain.png",
+            "lighting.png",
+            "night.png",
+            "partlycloudy.png",
+            "rainy.png",
+            "sleet.png",
+            "snowy.png",
+            "stormy.png",
+            "sunny.png",
+            "thunderstorm.png"
+    };
+
+    public WeatherIcon() {
+        this.weatherImage = new Image(new Texture(getRandomWeatherFileLocation()));
+        this.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        position();
+    }
+
+    public void position() {
+        weatherImage.setPosition(Gdx.graphics.getWidth()/2 - weatherImage.getWidth()/2, 128f);
+    }
+
+    public String getRandomWeatherFileLocation() {
+        int randomIndex = PseudoRandom.seedRandomInt(0, weatherFile.length);
+        return "image/"+weatherFile[randomIndex];
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        this.weatherImage.draw(batch, parentAlpha);
+    }
+}
---
+++ source/core/src/main/com/deco2800/game/components/weather/WeatherIconDisplay.java
@@ -0,0 +1,27 @@
+package com.deco2800.game.components.weather;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.deco2800.game.ui.UIComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WeatherIconDisplay extends UIComponent {
+    private static final Logger logger = LoggerFactory.getLogger(WeatherIconDisplay.class);
+    private WeatherIcon weatherIcon;
+
+    @Override
+    public void create() {
+        super.create();
+        addActors();
+    }
+
+
+    private void addActors() {
+        this.weatherIcon = new WeatherIcon();
+        stage.addActor(this.weatherIcon);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {}
+}
