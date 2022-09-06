84b1c7438dc5c398fc4d909149a42582e2afd8d9
DLAmeng <xzy1152241624@gmail.com>
2022-09-06 14:29:01 +1000

    Realize the animation of snake up, down, left and right

---
+++ source/core/src/main/com/deco2800/game/components/npc/EnemyAnimationController.java
@@ -0,0 +1,28 @@
+package com.deco2800.game.components.npc;
+
+import com.deco2800.game.components.Component;
+import com.deco2800.game.rendering.AnimationRenderComponent;
+
+/**
+ * This class listens to events relevant to enemy entity's state and plays the animation when one
+ * of the events is triggered.
+ */
+public class EnemyAnimationController extends Component {
+    AnimationRenderComponent animator;
+
+    @Override
+    public void create() {
+        super.create();
+        animator = this.entity.getComponent(AnimationRenderComponent.class);
+        entity.getEvents().addListener("goLeft", this::animateLeft);
+        entity.getEvents().addListener("goRight", this::animateRight);
+        entity.getEvents().addListener("goUp", this::animateUp);
+        entity.getEvents().addListener("goDown", this::animateDown);
+    }
+
+    public void animateLeft(){ animator.startAnimation("floatLeft"); }
+
+    public void animateRight(){ animator.startAnimation("floatLeft"); }
+    public void animateUp(){ animator.startAnimation("floatUp"); }
+    public void animateDown(){ animator.startAnimation("floatDown"); }
+}
