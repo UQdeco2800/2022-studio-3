87de6242a08a3be2b6865c0a312fb2a8fce89074
Angus Buchanan <angus1252@gmail.com>
2022-09-11 21:21:16 +1000

    Fixed some bugs for deselecting buildings and troops

--- source/core/src/main/com/deco2800/game/components/friendlyunits/SelectableComponent.java
+++ source/core/src/main/com/deco2800/game/components/friendlyunits/SelectableComponent.java
@@ -10,6 +10,7 @@ import com.deco2800.game.components.building.BuildingActions;
 import com.deco2800.game.input.InputComponent;
 import com.deco2800.game.physics.components.ColliderComponent;
 import com.deco2800.game.services.ServiceLocator;
+import org.slf4j.helpers.Util;
 
 import java.security.Provider;
 
@@ -47,6 +48,11 @@ public class SelectableComponent extends Component {
      * @param yCoordinate The y coordinate of the mouse
      */
     public void isIn(int xCoordinate, int yCoordinate) {
+        if (xCoordinate < 600 && yCoordinate > 500) {
+
+            return;
+        }
+
         Vector2 pointInWorld = screenToWorldPosition(xCoordinate, yCoordinate);
 
         // If entity is a building, test if the clicked point is in building collider
@@ -54,6 +60,7 @@ public class SelectableComponent extends Component {
             selected = entity.getComponent(ColliderComponent.class).getFixture().testPoint(pointInWorld);
             return;
         }
+
 
         Vector2 startPosition = entity.getPosition();
         Vector2 endPosition = entity.getPosition().mulAdd(entity.getScale(), 1f);
