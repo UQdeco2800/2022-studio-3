1ddfd02dbd89d9432139e36e19310ce2e231564e
Sourav-RC <rcsourav7@gmail.com>
2022-09-11 12:29:24 +0530

    Disappearing Info Box feature added

--- source/core/src/main/com/deco2800/game/components/maingame/InfoBoxDisplay.java
+++ source/core/src/main/com/deco2800/game/components/maingame/InfoBoxDisplay.java
@@ -68,7 +68,7 @@ public class InfoBoxDisplay extends UIComponent {
         backgroundBoxImage.setHeight((float) (initialHeight * 1.5));
         backgroundBoxImage.setPosition(0f, 0f);
 
-        stage.addActor(backgroundBoxImage);
+
 
         this.pictureTable = new Table();
         pictureTable.setWidth(135);
@@ -96,9 +96,7 @@ public class InfoBoxDisplay extends UIComponent {
         buildingTable.setHeight(100);
         buildingTable.setPosition(400, Gdx.graphics.getHeight()-680);
 
-        stage.addActor(pictureTable);
-        stage.addActor(infoTable);
-        stage.addActor(buildingTable);
+
         levelUpBtn.addListener(
                 new ChangeListener() {
                     @Override
@@ -147,6 +145,10 @@ public class InfoBoxDisplay extends UIComponent {
         boolean buildingSelected = false;
         //If there are entities selected
         if (!selectedEntities.isEmpty()) {
+            stage.addActor(backgroundBoxImage);
+            stage.addActor(pictureTable);
+            stage.addActor(infoTable);
+            stage.addActor(buildingTable);
             int length = selectedEntities.size;
             int sideLength = (int) Math.ceil(Math.sqrt(length));
             int column = 0;
@@ -211,6 +213,11 @@ public class InfoBoxDisplay extends UIComponent {
             }
 
 
+        }
+        else {
+            pictureTable.remove();
+            infoTable.remove();
+            backgroundBoxImage.remove();
         }
     }
 
