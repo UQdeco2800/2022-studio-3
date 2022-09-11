e4cecbe64f4d45c4a4e8c54c4c90be504a5b48f8
Corfliss <fauzanhanandito@gmail.com>
2022-09-11 16:51:12 +1000

    Writing a commented out test createIcon for WeatherIconTest

--- source/core/src/test/com/deco2800/game/components/weather/WeatherIconTest.java
+++ source/core/src/test/com/deco2800/game/components/weather/WeatherIconTest.java
@@ -7,18 +7,18 @@ import com.deco2800.game.extensions.GameExtension;
 import com.deco2800.game.utils.random.Timer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(GameExtension.class)
 public class WeatherIconTest {
+    Skin countdownSkin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
+    Timer timer = new Timer(10000, 10001);
+    Label timerCountdown = new Label(String.valueOf(timer.timeLeft()), countdownSkin);
+    WeatherIcon weatherIcon = new WeatherIcon(timerCountdown);
 
-    @Test
-    void createIcon() {
-        Skin countdownSkin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
-        Timer timer = new Timer(10000, 10001);
-        Label timerCountdown = new Label(String.valueOf(timer.timeLeft()), countdownSkin);
-        WeatherIcon weatherIcon = new WeatherIcon(timerCountdown);
-    }
+//    @Test
+//    void createIcon() {
+//        Label sampleCountdown = new Label(weatherIcon.getTimerLabel(), countdownSkin);
+//        assertEquals(weatherIcon, new WeatherIcon(sampleCountdown));
+//    }
 }
