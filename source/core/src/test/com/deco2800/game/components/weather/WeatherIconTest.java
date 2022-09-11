f86552ad9ec391c3059a8b9768c9ee30a27c4125
Corfliss <fauzanhanandito@gmail.com>
2022-09-11 17:13:16 +1000

    Added createIcon test in WeatherIcontest

--- source/core/src/test/com/deco2800/game/components/weather/WeatherIconTest.java
+++ source/core/src/test/com/deco2800/game/components/weather/WeatherIconTest.java
@@ -12,13 +12,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(GameExtension.class)
 public class WeatherIconTest {
     Skin countdownSkin = new Skin(Gdx.files.internal("flat-earth/skin/flat-earth-ui.json"));
-    Timer timer = new Timer(10000, 10001);
+    Timer timer = new Timer(5000, 10001);
     Label timerCountdown = new Label(String.valueOf(timer.timeLeft()), countdownSkin);
     WeatherIcon weatherIcon = new WeatherIcon(timerCountdown);
 
-//    @Test
-//    void createIcon() {
-//        Label sampleCountdown = new Label(weatherIcon.getTimerLabel(), countdownSkin);
-//        assertEquals(weatherIcon, new WeatherIcon(sampleCountdown));
-//    }
+    @Test
+    void createIcon() {
+        assertNotNull(weatherIcon);
+    }
 }
