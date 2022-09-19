c67a5102edad8a7cea81dbc288d4ce3657b62fd4
HJPearce <haydenpearce0@gmail.com>
2022-09-20 04:08:24 +1000

    Making the base unit config extend the base entity config and setting the default health to 100

--- source/core/src/main/com/deco2800/game/entities/configs/BaseUnitConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/BaseUnitConfig.java
@@ -1,10 +1,7 @@
 package com.deco2800.game.entities.configs;
 
-public class BaseUnitConfig {
+public class BaseUnitConfig extends BaseEntityConfig {
     public int troops = 1;
-    public int health = 100;
-    public int baseAttack = 0;
-    public int baseDefence = 0;
     public float landSpeed = 1.0f; // potentially unused
     public float movementRadius = 1f;
     public int range = 0;
