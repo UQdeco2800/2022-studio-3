2fa16d2c00f84661ddeb9a536290f8a2118005cc
usrnatc <n.corcoran@uqconnect.edu>
2022-09-27 12:35:57 +1000

    Added skeleton game area for tutorial

---
+++ source/core/src/main/com/deco2800/game/areas/TutorialGameArea.java
@@ -0,0 +1,9 @@
+package com.deco2800.game.areas;
+
+public class TutorialGameArea extends GameArea {
+
+    @Override
+    public void create() {
+        /* TODO: create necessary terrain and objects */
+    }
+}
