2304fec808d512db172665fd6f4b77d034599ad5
lpolicar <lpolicar@lpolicarsiMac2.modem>
2022-09-28 13:53:14 +1000

    Added components for Builder and a builder factory

---
+++ source/core/src/main/com/deco2800/game/worker/type/BuilderFactory.java
@@ -0,0 +1,41 @@
+package com.deco2800.game.worker.type;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.files.FileLoader;
+import com.deco2800.game.worker.components.BuilderAnimationController;
+import com.deco2800.game.worker.components.CollectStatsComponent;
+import com.deco2800.game.worker.components.type.ForagerComponent;
+import com.deco2800.game.rendering.AnimationRenderComponent;
+import com.deco2800.game.services.ServiceLocator;
+import com.deco2800.game.worker.WorkerConfig;
+import com.deco2800.game.worker.WorkerFactory;
+
+public class BuilderFactory {
+    private static final WorkerConfig stats = FileLoader.readClass(WorkerConfig.class, "configs/worker.json");
+
+    public static Entity createBuilder() {
+        AnimationRenderComponent animator =
+                new AnimationRenderComponent(
+                        ServiceLocator.getResourceService()
+                                .getAsset("images/forager_forward.atlas", TextureAtlas.class));
+
+        animator.addAnimation("forager_forward_idle", 0.1f, Animation.PlayMode.LOOP);
+        animator.addAnimation("forager_forward_move", 0.1f, Animation.PlayMode.LOOP);
+        animator.addAnimation("foragerActionRight", 0.1f, Animation.PlayMode.LOOP);
+        animator.addAnimation("foragerActionLeft", 0.1f, Animation.PlayMode.LOOP);
+        animator.addAnimation("foragerRight", 0.1f, Animation.PlayMode.LOOP);
+        animator.addAnimation("foragerLeft", 0.1f, Animation.PlayMode.LOOP);
+
+        Entity builder = WorkerFactory.createWorker()
+                .addComponent(new ForagerComponent())
+                .addComponent(new CollectStatsComponent(2))
+                .addComponent(animator)
+                .addComponent(new BuilderAnimationController());
+        builder.getComponent(AnimationRenderComponent.class).scaleEntity();
+        builder.scaleHeight(1.5f);
+        builder.scaleWidth(1.5f);
+        return builder;
+    }
+}
