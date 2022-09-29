b325fc5d1814868992ade7ecef11e42902058ec0
Harikesha Suresh <harikeshasuresh@gmail.com>
2022-09-29 14:54:38 +1000

    Fixed issues with previous commit

--- source/core/src/main/com/deco2800/game/entities/configs/TrebuchetConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/TrebuchetConfig.java
@@ -4,5 +4,8 @@ import com.deco2800.game.components.building.Building;
 
 public class TrebuchetConfig extends BaseEntityConfig {
     public Building type = Building.TREBUCHET;
+
     public int level = 1;
+    public float range = 0f;
+    public float sight = 0f;
 }
