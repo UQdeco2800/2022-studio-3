508b59dd71b465c28fbe72cb53f30656febcabaf
Corfliss <fauzanhanandito@gmail.com>
2022-10-03 19:48:33 +1000

    Slowing down the flooding

--- source/core/src/main/com/deco2800/game/areas/MapGenerator/FloodingGenerator.java
+++ source/core/src/main/com/deco2800/game/areas/MapGenerator/FloodingGenerator.java
@@ -38,7 +38,7 @@ public class FloodingGenerator extends Component {
         if (this.timer.isTimerExpired()) {
             triggerFloodEvent();
             this.atlantisGameArea.flood();
-            this.timer = new Timer(10, 11);
+            this.timer = new Timer(500, 501);
             this.timer.start();
         }
     }
@@ -76,6 +76,8 @@ public class FloodingGenerator extends Component {
         coords[0] = PseudoRandom.seedRandomInt(1,89);
         coords[1] = PseudoRandom.seedRandomInt(1,199);
         return coords;
+
+
     }
 }
 
