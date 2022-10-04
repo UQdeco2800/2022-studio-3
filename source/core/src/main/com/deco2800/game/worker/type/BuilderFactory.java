0dadf1a0497b641c7251a016deb9f9156e7cf557
HaroldHS <haroldhs.hh@gmail.com>
2022-10-04 13:50:15 +1000

    Remove unused modules

--- source/core/src/main/com/deco2800/game/worker/type/BuilderFactory.java
+++ source/core/src/main/com/deco2800/game/worker/type/BuilderFactory.java
@@ -3,18 +3,15 @@ package com.deco2800.game.worker.type;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.deco2800.game.entities.Entity;
-import com.deco2800.game.files.FileLoader;
 import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.worker.components.BuilderAnimationController;
 import com.deco2800.game.worker.components.BuildingFixComponent;
 import com.deco2800.game.worker.components.type.BuilderComponent;
 import com.deco2800.game.rendering.AnimationRenderComponent;
 import com.deco2800.game.services.ServiceLocator;
-import com.deco2800.game.worker.WorkerConfig;
 import com.deco2800.game.worker.WorkerFactory;
 
 public class BuilderFactory {
-    private static final WorkerConfig stats = FileLoader.readClass(WorkerConfig.class, "configs/worker.json");
 
     public static Entity createBuilder() {
         AnimationRenderComponent animator =
