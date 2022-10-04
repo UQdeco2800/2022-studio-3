0c8ce7f7b8d0bbcd9abf84a5b7ff1d7af3db8cd2
JiashengLi <88020895+JiashengLi@users.noreply.github.com>
2022-10-04 09:51:01 +0800

    Revert "Revert "Merge branch 'team1s3' of https://github.com/UQdeco2800/2022-studio-3 into team1s3""
    This reverts commit 0abd506853058c86ba6fffa6bba5b4ceac0b421c.

--- source/core/src/main/com/deco2800/game/components/weather/WeatherIconProperties.java
+++ source/core/src/main/com/deco2800/game/components/weather/WeatherIconProperties.java
@@ -1,4 +1,4 @@
-package main.com.deco2800.game.components.weather;
+package com.deco2800.game.components.weather;
 
 public enum WeatherIconProperties{
     // Does not affect movement, affect lighting of the environment
