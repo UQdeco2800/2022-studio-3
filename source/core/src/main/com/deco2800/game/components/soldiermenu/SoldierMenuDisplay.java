c565d43b6f96e1d6e48b32f11a4386c2c955fff1
Garryco Thandi <garrycoth1010@gmail.com>
2022-10-08 23:38:23 +1000

    Adding soldiers avatar on character selection menu

--- source/core/src/main/com/deco2800/game/components/soldiermenu/SoldierMenuDisplay.java
+++ source/core/src/main/com/deco2800/game/components/soldiermenu/SoldierMenuDisplay.java
@@ -9,6 +9,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class SoldierMenuDisplay extends UIComponent {
     private Image backgroundTexture;
+    private Image archerTexture;
+    private Image swordsmanTexture;
+    private Image spearmanTexture;
+    private Image hopliteTexture;
     float initHeight;
     float initWidth;
     @Override
@@ -20,13 +24,36 @@ public class SoldierMenuDisplay extends UIComponent {
     private void addActors() {
         Texture texture = ServiceLocator.
                 getResourceService().getAsset("images/character-selection-menu.png", Texture.class);
+        Texture archer = ServiceLocator.
+                getResourceService().getAsset("images/archer_avatar.png", Texture.class);
+        Texture swordsman = ServiceLocator.
+                getResourceService().getAsset("images/swordsman_avatar.png", Texture.class);
+        Texture spearman = ServiceLocator.
+                getResourceService().getAsset("images/spearman_avatar.png", Texture.class);
+        Texture hoplite = ServiceLocator.
+                getResourceService().getAsset("images/hoplite_avatar.png", Texture.class);
 
         backgroundTexture = new Image(texture);
+        archerTexture = new Image(archer);
+        swordsmanTexture = new Image(swordsman);
+        spearmanTexture = new Image(spearman);
+        hopliteTexture = new Image(hoplite);
+
         this.initHeight = backgroundTexture.getHeight();
         this.initWidth = backgroundTexture.getWidth();
         backgroundTexture.setSize(663, 330);
 
+        archerTexture.setSize(100,100);
+        swordsmanTexture.setSize(100,100);
+        spearmanTexture.setSize(100,100);
+        hopliteTexture.setSize(100,100);
+
+
         stage.addActor(backgroundTexture);
+        stage.addActor(archerTexture);
+        stage.addActor(swordsmanTexture);
+        stage.addActor(spearmanTexture);
+        stage.addActor(hopliteTexture);
         /*
         backgroundShopImage = new Image(texture);
         this.initHeight = backgroundShopImage.getHeight();
@@ -45,13 +72,20 @@ public class SoldierMenuDisplay extends UIComponent {
     protected void draw(SpriteBatch batch) {
         int screenHeight = Gdx.graphics.getHeight();
         int screenWidth = Gdx.graphics.getWidth();
-        float offsetX = 1000f;
-        float offsetY = 800f;
-        backgroundTexture.setPosition(screenWidth - offsetX, screenHeight - offsetY);
+        backgroundTexture.setPosition(screenWidth - screenWidth*3/4,0);
+        archerTexture.setPosition(screenWidth*15/45, screenHeight/6);
+        swordsmanTexture.setPosition(screenWidth*19/45, screenHeight/6);
+        spearmanTexture.setPosition(screenWidth*23/45, screenHeight/6);
+        hopliteTexture.setPosition(screenWidth*27/45, screenHeight/6);
+
     }
     @Override
     public void dispose() {
         super.dispose();
         backgroundTexture.remove();
+        archerTexture.remove();
+        swordsmanTexture.remove();
+        spearmanTexture.remove();
+        hopliteTexture.remove();
     }
 }
