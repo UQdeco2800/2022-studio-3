9baa6861466925848ff6784b42eb5d05b8b6d673
usrnatc <n.corcoran@uqconnect.edu>
2022-10-10 19:34:26 +1000

    Context box has new UI removed old texture assets added new tetxure asset

--- source/core/src/main/com/deco2800/game/components/BuildingUIDataComponent.java
+++ source/core/src/main/com/deco2800/game/components/BuildingUIDataComponent.java
@@ -1,13 +1,9 @@
 package com.deco2800.game.components;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
 import com.deco2800.game.components.building.BuildingActions;
 import com.deco2800.game.components.friendlyunits.SelectableComponent;
 import com.deco2800.game.rendering.TextureRenderComponent;
@@ -15,22 +11,15 @@ import com.deco2800.game.services.ResourceService;
 import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 
-import java.util.ArrayList;
-
 public class BuildingUIDataComponent extends UIComponent {
     private CombatStatsComponent combatStats;
     private BuildingActions buildingInfo;
     private SelectableComponent selectableComponent;
     private boolean isSelected = false;
-    private static Table infoTable;
-    private static Image flagSprite;
-    private static Image columnBaseSprite;
-    private static Image columnConnectorSprite;
-    private ArrayList<Label> buildingStats;
+    private static Image contextBoxSprite;
+    private Group contextBoxItems;
     private final String[] textures = {
-            "images/context_flag_90x90.png",
-            "images/greek_column_base_90x90.png",
-            "images/greek_column_connector_90x90.png",
+            "images/context_box.png",
     };
 
     private void loadAssets() {
@@ -51,106 +40,68 @@ public class BuildingUIDataComponent extends UIComponent {
         buildingInfo = this.entity.getComponent(BuildingActions.class);
         selectableComponent = this.entity.getComponent(SelectableComponent.class);
         isSelected = selectableComponent.isSelected();
+        contextBoxItems = new Group();
 
-        Texture flagTexture = ServiceLocator.getResourceService()
-                .getAsset("images/context_flag_90x90.png", Texture.class);
-        Texture columnBaseTexture = ServiceLocator.getResourceService()
-                .getAsset("images/greek_column_base_90x90.png", Texture.class);
-        Texture columnConnectorTexture = ServiceLocator.getResourceService()
-                .getAsset("images/greek_column_connector_90x90.png", Texture.class);
+        Texture contextBoxTexture = ServiceLocator
+                .getResourceService().getAsset("images/context_box.png", Texture.class);
 
-        infoTable = new Table();
-        flagSprite = new Image(flagTexture);
-        columnBaseSprite = new Image(columnBaseTexture);
-        columnConnectorSprite = new Image(columnConnectorTexture);
-        this.buildingStats = new ArrayList<>();
+        contextBoxSprite = new Image(contextBoxTexture);
     }
 
     /**
-     * Creates and returns a table containing the context information of the currently selected building
-     *
-     * @param screenWidth  - current width of screen
-     * @param screenHeight - current height of screen
-     * @return - table containing context information of currently selected building
-     * <p>
-     * Note: If the current screen size is below a certain size, a compact version of the context table is returned
-     */
-    private Table generateTable(float screenWidth, float screenHeight) {
-
-        Table table = new Table();
-        /* TODO: access selected building name */
-        Label name = new Label("BUILD_NAME", skin);
-        VerticalGroup stats = new VerticalGroup();
-        Pixmap tableBackground;
-        Image building = new Image(ServiceLocator.getResourceService()
-                .getAsset(entity.getComponent(TextureRenderComponent.class).texturePath, Texture.class));
-
-        for (Label stat : this.buildingStats) {
-
-            stats.addActor(stat);
-        }
-
-        if (screenHeight <= 540f || screenWidth <= 960f) {
-
-            tableBackground = new Pixmap(390, 180, Pixmap.Format.RGB565);
-
-            table.setSize(390, 180);
-            table.add(name).expand().colspan(2).center().row();
-            table.add(building).expand().width(180).height(180).fill();
-            table.add(stats).expand().center();
-        } else {
-
-            HorizontalGroup titleRow = new HorizontalGroup().fill();
-            VerticalGroup columns = new VerticalGroup().fill();
-            tableBackground = new Pixmap(480, 360, Pixmap.Format.RGB565);
-
-            table.setSize(480, 360);
-            titleRow.addActor(flagSprite);
-
-            columns.addActor(columnConnectorSprite);
-            columns.addActor(columnBaseSprite);
-
-            /* TODO: add close button on far right */
-            table.add(flagSprite).expand().left().top().width(90).height(90);
-            table.add(name);
-            table.add(columnBaseSprite).expand().right().top().row();
-            table.add(building).expand().left().width(180).height(180);
-            table.add(stats).expand().center();
-            /* FIXME: column_base does not show */
-            table.add(columns).expand().right().bottom();
-        } /* screenHeight > 540 && screenWidth > 960 */
-
-        tableBackground.setColor(25, 25, 25, 1);
-        tableBackground.fill();
-        table.setBackground(new TextureRegionDrawable(new TextureRegion(new Texture(tableBackground))));
-        table.pack();
-        table.setPosition(0f, 0f, Align.bottomLeft);
-        return (table);
-    }
-
-    /**
-     * Draw the renderable. Should be called only by the renderer, not manually.
+     * Draw the render-able. Should be called only by the renderer, not manually.
      *
      * @param batch Batch to render to.
      */
     @Override
     protected void draw(SpriteBatch batch) {
 
-        if (selectableComponent != null) {
+        /* TODO:
+                - get building attributes and display
+                - get building name and display
+                - get building actions and generate buttons accordingly
+         */
+        Label attributesLabel;
+        Label inventoryLabel;
+        Label statsLabel;
+        Image buildingImage;
+
+        if (selectableComponent != null)
             isSelected = selectableComponent.isSelected();
-        }
+
         if (isSelected) {
 
-            buildingStats.clear();
-            buildingStats.add(new Label("Health: " + combatStats.getHealth(), skin));
-            buildingStats.add(new Label("Level: " + buildingInfo.getLevel(), skin));
+            statsLabel = new Label("Stats: ", skin);
+            attributesLabel = new Label("Health:  0 Random: 0", skin);
+            inventoryLabel = new Label("inventory things...", skin);
+            buildingImage = new Image(ServiceLocator.getResourceService()
+                    .getAsset(entity.getComponent(TextureRenderComponent.class).texturePath, Texture.class));
 
-            infoTable.clear();
-            infoTable = generateTable(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            stage.addActor(infoTable);
+            /* TODO: create small context box for smaller window sizes */
+            contextBoxItems.setPosition(contextBoxSprite.getX(), contextBoxSprite.getY());
+            contextBoxItems.setSize(contextBoxSprite.getWidth(), contextBoxSprite.getHeight());
+            statsLabel.setPosition(14f, 239f);
+            statsLabel.setSize(158f, 25f);
+            statsLabel.setWrap(true);
+            attributesLabel.setPosition(14f, 11f);
+            attributesLabel.setSize(158f, 217f);
+            attributesLabel.setWrap(true);
+            inventoryLabel.setPosition(184f, 68f);
+            inventoryLabel.setSize(208f, 85f);
+            inventoryLabel.setWrap(true);
+            buildingImage.setPosition(183f, 166f);
+            buildingImage.setSize(209f, 96f);
+
+            contextBoxItems.addActor(statsLabel);
+            contextBoxItems.addActor(attributesLabel);
+            contextBoxItems.addActor(buildingImage);
+            contextBoxItems.addActor(inventoryLabel);
+            contextBoxItems.addActor(contextBoxSprite);
+
+            stage.addActor(contextBoxItems);
         } else {
 
-            infoTable.remove();
+            contextBoxItems.remove();
         }
     }
 }
