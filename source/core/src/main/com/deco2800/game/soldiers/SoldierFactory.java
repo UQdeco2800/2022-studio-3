54b91831676ba4562612ebc40163da3724f46fc4
Lorenzo Policar <lorenzo_policar@yahoo.com>
2022-10-13 13:21:00 +1000

    Added Base Factory for Soldier, added components for each soldier type, added tasks for idling for soldier

---
+++ source/core/src/main/com/deco2800/game/soldiers/SoldierFactory.java
@@ -0,0 +1,53 @@
+package com.deco2800.game.soldiers;
+
+import com.deco2800.game.ai.tasks.AITaskComponent;
+import com.deco2800.game.components.CombatStatsComponent;
+import com.deco2800.game.components.EntityType;
+import com.deco2800.game.components.HealthBarComponent;
+import com.deco2800.game.components.TouchAttackComponent;
+import com.deco2800.game.components.friendly.FriendlyComponent;
+import com.deco2800.game.components.friendlyunits.SelectableComponent;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.configs.UnitConfigs;
+import com.deco2800.game.files.FileLoader;
+import com.deco2800.game.input.InputComponent;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.PhysicsUtils;
+import com.deco2800.game.physics.components.ColliderComponent;
+import com.deco2800.game.physics.components.HitboxComponent;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.physics.components.PhysicsMovementComponent;
+import com.deco2800.game.services.ServiceLocator;
+import com.deco2800.game.soldiers.movement.SoldierIdleTask;
+
+public class SoldierFactory {
+
+    private static final UnitConfigs stats = FileLoader.readClass(UnitConfigs.class, "configs/units.json");
+
+    public static Entity creatSoldier() {
+        InputComponent inputComponent =
+                ServiceLocator.getInputService().getInputFactory().createForFriendlyUnit();
+        AITaskComponent aiComponent = new AITaskComponent().addTask(new SoldierIdleTask());
+        Entity soldier =
+                new Entity()
+                        .addComponent(new PhysicsComponent())
+                        .addComponent(new PhysicsMovementComponent())
+                        .addComponent(new ColliderComponent())
+                        .addComponent(new HitboxComponent().setLayer(PhysicsLayer.SOLDIER))
+                        .addComponent(new FriendlyComponent())
+                        .addComponent(new SelectableComponent())
+                        //.addComponent(new CombatStatsComponent(stats.example.health, stats.example.baseAttack, stats.example.baseDefence))
+                        .addComponent(new HealthBarComponent(EntityType.FRIENDLY))
+                        .addComponent(aiComponent)
+                        .addComponent(inputComponent)
+                        .addComponent(new TouchAttackComponent(PhysicsLayer.NPC));
+
+        PhysicsUtils.setScaledCollider(soldier, 0.6f, 0.3f);
+        soldier.getComponent(ColliderComponent.class).setDensity(1.5f);
+        return soldier;
+    }
+
+    private SoldierFactory() {
+        throw new IllegalStateException("Instantiating static util class");
+    }
+}
---
+++ source/core/src/main/com/deco2800/game/soldiers/movement/SoldierIdleTask.java
@@ -0,0 +1,91 @@
+package com.deco2800.game.soldiers.movement;
+
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
+import java.util.List;
+import com.deco2800.game.ai.tasks.DefaultTask;
+import com.deco2800.game.ai.tasks.PriorityTask;
+import com.deco2800.game.worker.components.movement.WorkerMovementTask;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default state of the worker when they have nothing to do. Waits until a movement task is triggered.
+ */
+public class SoldierIdleTask extends DefaultTask implements PriorityTask {
+    private static final Logger logger = LoggerFactory.getLogger(SoldierIdleTask.class);
+    private boolean idling = false;
+    private WorkerMovementTask movementTask;
+    private Vector2 startPos;
+
+    public SoldierIdleTask() { /* already has start */ }
+
+    @Override
+    public int getPriority() {
+        return 2; // Low priority task
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        startPos = owner.getEntity().getPosition();
+        movementTask = new WorkerMovementTask(startPos);
+        movementTask.create(owner);
+        movementTask.start();
+        owner.getEntity().getEvents().addListener("soldierWalk", this::startMoving);
+    }
+
+    @Override
+    public void update() {
+        if (movementTask.isMoving()) {
+            movementTask.update();
+        } else if (!idling) {
+            // Start idling again
+            owner.getEntity().getEvents().trigger("soldierIdleAnimate");
+            idling = true;
+        }
+    }
+
+    /**
+     * Moves the soldier to the target
+     *
+     * @param target target of the soldier
+     */
+    public void startMoving(Vector2 target) {
+        movementTask.stop();
+        movementTask.setTarget(target);
+        movementTask.start();
+
+        if (target.x > owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y){
+            owner.getEntity().getEvents().trigger("soldierBackRightMove");
+        } else if (target.x < owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("soldierBackLeftMove");
+        } else if (target.x < owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("soldierForwardLeftMove");
+        } else if (target.x > owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("soldierForwardRightMove");
+        } else {
+            owner.getEntity().getEvents().trigger("soldierIdleAnimate");
+        }
+        idling = false;
+    }
+
+    /**
+     * Return the current movement task
+     *
+     * @return the movement task
+     */
+    public WorkerMovementTask getMovementTask() {
+        return this.movementTask;
+    }
+
+    /**
+     * Return whether the worker is currently idling.
+     *
+     * @return True if the worker is stationary. False otherwise.
+     */
+    public boolean isIdling() {
+        return this.idling;
+    }
+}
