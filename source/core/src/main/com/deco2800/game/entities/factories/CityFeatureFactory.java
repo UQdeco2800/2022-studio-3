f892820bdc1b7d0f968dabb1c2726a4f0ab60833
rjg1 <rj.gibbons12@gmail.com>
2022-10-14 18:28:25 +1000

    Started writing CityFeatureFactory to produce entities that work as aesthetic additions to the city

---
+++ source/core/src/main/com/deco2800/game/entities/factories/CityFeatureFactory.java
@@ -0,0 +1,20 @@
+package com.deco2800.game.entities.factories;
+
+import com.deco2800.game.entities.Entity;
+
+public class CityFeatureFactory {
+    public static Entity createLamp() {
+        Entity lamp = new Entity();
+        //Add components
+        //lamp.addComponent()
+        return null;
+    }
+
+    public static Entity createWaterFeature() {
+        return null;
+    }
+
+    public static Entity createCityBush() {
+        return null;
+    }
+}
