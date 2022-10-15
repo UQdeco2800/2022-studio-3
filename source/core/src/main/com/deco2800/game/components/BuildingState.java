3665055f7d251c74cf63006ce611ab417ff44deb
Harikesha Suresh <harikeshasuresh@gmail.com>
2022-10-15 11:47:27 +1000

    Working on refining building animations (#250)

---
+++ source/core/src/main/com/deco2800/game/components/BuildingState.java
@@ -0,0 +1,6 @@
+package com.deco2800.game.components;
+
+public enum BuildingState {
+    FULL_HEALTH,
+    HALF_HEALTH,
+}
--- source/core/src/main/com/deco2800/game/components/building/BuildingAnimationController.java
+++ source/core/src/main/com/deco2800/game/components/building/BuildingAnimationController.java
@@ -7,10 +7,22 @@ import com.deco2800.game.rendering.AnimationRenderComponent;
 import com.deco2800.game.services.GameTime;
 import com.deco2800.game.services.ServiceLocator;
 
+/**
+ * Component to handle building animations, this component
+ * simply listens to events and plays animation associated
+ * with events called.
+ */
 public class BuildingAnimationController extends Component {
     AnimationRenderComponent animator;
     private int D_health;
     private boolean dispose = false;
+
+    private static final String HALF_HEALTH = "50-idle";
+    private static final String HALF_HEALTH_TRANSITION = "50";
+    private static final String FULL_HEALTH = "100";
+    private static final String FULL_ATTACKED = "attacked";
+    private static final String COLLAPSE = "collapse";
+    private static final String REBUILD = "reconstruction";
 
     @Override
     public void create() {
@@ -18,6 +30,14 @@ public class BuildingAnimationController extends Component {
         animator = this.entity.getComponent(AnimationRenderComponent.class);
         D_health = this.entity.getComponent(CombatStatsComponent.class).getHealth();
         entity.getEvents().addListener("HealthAnimation", this::updateAnimation);
+
+        // Damage transition events
+        entity.getEvents().addListener("damaged", this::damagedTransition);
+        entity.getEvents().addListener("collapsing", this::collapseTransition);
+        entity.getEvents().addListener("creation", this::creationTransition);
+        // Under attack transition
+        entity.getEvents().addListener("underAttackFull", this::underAttackFull);
+        entity.getEvents().addListener("underAttackHalf", this::underAttackHalf);
     }
 
     void updateAnimation() {
@@ -38,5 +58,49 @@ public class BuildingAnimationController extends Component {
         if (this.enabled) {
             animator.startAnimation(entity.getComponent(HealthAnimation.class).getAnimation());
         }
+    }
+
+    /**
+     * This method plays an animation that transitions
+     * the building to its damaged state.
+     */
+    private void damagedTransition() {
+        animator.startAnimation(HALF_HEALTH_TRANSITION);
+        animator.startAnimation(HALF_HEALTH);
+    }
+
+    /**
+     * This method plays an animation that transitions
+     * the building to its collapsed state.
+     */
+    private void collapseTransition() {
+        animator.startAnimation(COLLAPSE);
+    }
+
+    /**
+     * This method plays an animation when building
+     * is being created/constructed/reconstructed.
+     */
+    private void creationTransition() {
+        animator.startAnimation(REBUILD);
+        animator.startAnimation(FULL_HEALTH);
+    }
+
+    /**
+     * This method plays an animation when building
+     * health is greater than 50% and is being
+     * attacked
+     */
+    private void underAttackFull() {
+        animator.startAnimation(FULL_ATTACKED);
+    }
+
+    /**
+     * This method plays an animation when building
+     * health is greater lesser than 50% (inclusive)
+     * and is being attacked.
+     */
+    private void underAttackHalf() {
+        // animator.startAnimation(HALF_ATTACKED);
     }
 }
