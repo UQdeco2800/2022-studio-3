153e75a564c98f1882fe6de68bd749b97ca520e2
HaroldHS <haroldhs.hh@gmail.com>
2022-10-18 08:28:30 +1000

    Revert units movement, use the old movement mechanism

--- source/core/src/main/com/deco2800/game/worker/components/movement/WorkerIdleTask.java
+++ source/core/src/main/com/deco2800/game/worker/components/movement/WorkerIdleTask.java
@@ -2,23 +2,16 @@ package com.deco2800.game.worker.components.movement;
 
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
-import java.util.List;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.deco2800.game.ai.tasks.DefaultTask;
 import com.deco2800.game.ai.tasks.PriorityTask;
 import com.deco2800.game.components.friendlyunits.SelectableComponent;
-import com.deco2800.game.map.MapService;
-import com.deco2800.game.services.ServiceLocator;
-
-import java.security.Provider.Service;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Default state of the worker when they have nothing to do. Waits until a movement task is triggered.
- */
-public class WorkerIdleTask extends DefaultTask implements PriorityTask {
+
+public class WorkerIdleTask extends DefaultTask implements PriorityTask{
     private static final Logger logger = LoggerFactory.getLogger(WorkerIdleTask.class);
     private boolean idling = false;
     private WorkerMovementTask movementTask;
@@ -40,12 +33,13 @@ public class WorkerIdleTask extends DefaultTask implements PriorityTask {
     @Override
     public void start() {
         super.start();
-        selectableComponent = owner.getEntity().getComponent(SelectableComponent.class);
         startPos = owner.getEntity().getPosition();
         movementTask = new WorkerMovementTask(startPos);
         movementTask.create(owner);
         movementTask.start();
         owner.getEntity().getEvents().addListener("workerWalk", this::startMoving);
+
+        selectableComponent = owner.getEntity().getComponent(SelectableComponent.class);
     }
 
     @Override
@@ -56,21 +50,21 @@ public class WorkerIdleTask extends DefaultTask implements PriorityTask {
             isHovered = selectableComponent.isHovered();
         }
 
-        if(isHovered || isSelected){
-            if (movementTask.isMoving()) {
-                movementTask.update();
-            } else if (!idling) {
-                // Start idling again
+        if (movementTask.isMoving()) {
+            movementTask.update();
+        } else if (!idling) {
+            // Start idling again
+            if(isHovered || isSelected){
                 owner.getEntity().getEvents().trigger("workerHighlightedIdleAnimate");
-                idling = true;
-            }
-        }else{
-            if (movementTask.isMoving()) {
-                movementTask.update();
-            } else if (!idling) {
-                // Start idling again
+            }else{
                 owner.getEntity().getEvents().trigger("workerIdleAnimate");
-                idling = true;
+            }
+            idling = true;
+        } else if(idling){
+            if(isHovered || isSelected){
+                owner.getEntity().getEvents().trigger("workerHighlightedIdleAnimate");
+            }else{
+                owner.getEntity().getEvents().trigger("workerIdleAnimate");
             }
         }
     }
@@ -81,47 +75,24 @@ public class WorkerIdleTask extends DefaultTask implements PriorityTask {
      * @param target target of the worker
      */
     public void startMoving(Vector2 target) {
-
-        if (selectableComponent != null) {
-            isSelected = selectableComponent.isSelected();
-            isHovered = selectableComponent.isHovered();
-        }
-
         logger.debug("Starting moving to {}", target);
         movementTask.stop();
         movementTask.setTarget(target);
         movementTask.start();
-        
-        // Get the vector of the target and the worker
-        // If target's X & Y point is bigger than the worker's X point, call the back right animation
-        // If target's X point is smaller than the worker's X point but target's Y > worker's Y point, call the left animation
-        if(isHovered || isSelected){
-            if (target.x > owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y){
-                owner.getEntity().getEvents().trigger("workerHighlightedBackRightMove");
-            } else if (target.x < owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerHighlightedBackLeftMove");
-            } else if (target.x < owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerHighlightedForwardLeftMove");
-            } else if (target.x > owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerHighlightedForwardRightMove");
-            } else {
-                owner.getEntity().getEvents().trigger("workerHighlightedIdleAnimate");
-            }
-            idling = false;
-        }else{
-            if (target.x > owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y){
-                owner.getEntity().getEvents().trigger("workerBackRightMove");
-            } else if (target.x < owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerBackLeftMove");
-            } else if (target.x < owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerForwardLeftMove");
-            } else if (target.x > owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
-                owner.getEntity().getEvents().trigger("workerForwardRightMove");
-            } else {
-                owner.getEntity().getEvents().trigger("workerIdleAnimate");
-            }
-            idling = false;
+
+        // Get the vector of the target and the worker and trigger movement animation
+        if (target.x > owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y){
+            owner.getEntity().getEvents().trigger("workerHighlightedBackwardRightMoveAnimate");
+        } else if (target.x < owner.getEntity().getPosition().x && target.y > owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("workerHighlightedBackwardLeftMoveAnimate");
+        } else if (target.x < owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("workerHighlightedLeftMoveAnimate");
+        } else if (target.x > owner.getEntity().getPosition().x && target.y < owner.getEntity().getPosition().y) {
+            owner.getEntity().getEvents().trigger("workerHighlightedRightMoveAnimate");
+        } else {
+            owner.getEntity().getEvents().trigger("workerHighlightedIdleAnimate");
         }
+        idling = false;
     }
 
     /**
